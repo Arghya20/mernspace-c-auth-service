c0174082234bc0abd7a3545cd8710f5b06b62131
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-09-16 20:43:35 +0530

    add express js

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import app from './app';
+import { Config } from './config';
+
+const startServer = () => {
+    const PORT = Config.PORT;
+
+    try {
+        // eslint-disable-next-line no-console
+        app.listen(PORT, () => console.log(`listen on port ${PORT}`));
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+startServer();
