44f02479702af21e1fd06afd67b845277146b65c
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-09-16 20:11:59 +0530

    add config setup

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-function login(username: string): string {
-    const user = {
-        name: 'ram uppadhay',
-    };
-    const name = user['name'];
-    return username + name;
-}
-
-login('sam');
